ef41a182f77142428f81b888561616c627ad6aa9
unknown <bubbastinx@gmail.com>
2018-07-27 22:41:41 -0600

    added methods and key:value pairs to object

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,14 @@
-console.log("Script is connected!")
+
+const hangmanObj = {
+    wordbank: ['foxx', 'pryor', 'rock', 'hope', 'hart', 'vandyke', 'seinfeld', 'mac'],
+    blankBank: [],
+    letterBank: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ],
+    counter: 0,
+    gameScore: 0,
+    counterAdder(x) {
+       return  console.log('This is from the counterAdder!')
+    }
+}
+
+
+hangmanObj.counterAdder();

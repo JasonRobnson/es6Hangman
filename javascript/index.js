934f6458d3e4ba5094c457ef9ab62f67ede2b6b7
unknown <bubbastinx@gmail.com>
2018-07-31 10:31:35 -0600

    reduced document getElement by id to one method within Object

--- javascript/index.js
+++ javascript/index.js
@@ -4,20 +4,23 @@ const hangmanObj = {
     answerBank: [],
     blankBank: [],
     letterBank: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ],
-    usedLetterBank: [], 
+    usedLetterBank: ['a', 'b'], 
     counter: 0,
     guessLeft: 0, 
     gameWon: 0,
-    counterAdder(x) {
-       return  console.log('This is from the counterAdder!');
+    documentWriter(idTag, objKeyPropPair) {
+        document.getElementById(idTag).innerText = objKeyPropPair
     },
     revealBlanks(x) {
         return console.log('This reveals the blanks of the hidden answers!');
     },
     startGame(x) {
+        this.documentWriter('usedLetters', this.usedLetterBank);
+        this.documentWriter('gameScore', this.counter)
         this.numberGenerator();
         this.answerSetup(hangmanWord);
         this.createUnderscores(hangmanWord);
+        this.availableLetterHandler(hangmanObj.letterBank);
     },
     keyListener() {
         document.addEventListener('keypress', (event) => {
@@ -38,6 +41,9 @@ const hangmanObj = {
     numberGenerator() {
        let randomNumber = Math.floor(Math.random() * hangmanObj.wordbank.length)
        return randomNumber;
+    },
+    availableLetterHandler(letterBank) {
+        document.getElementById("lettersAvailable").innerText = this.letterBank.toLocaleString().toUpperCase()
     }
 }
 

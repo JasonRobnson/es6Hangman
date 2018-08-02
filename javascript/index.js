5b477bffce2920dd47a46f52457a805c6d5141ea
unknown <bubbastinx@gmail.com>
2018-08-02 16:10:53 -0600

    working on logic for comparing answer array and blank array

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,7 @@
 const hangmanObj = {
     wordbank: ['foxx', 'pryor', 'rock', 'hope', 'hart', 'vandyke', 'seinfeld', 'mac'],
     answerBank: [],
+    displayAnswerBank: [],
     blankBank: [],
     letterBank: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ],
     usedLetterBank: ['a', 'b'], 
@@ -15,36 +16,48 @@ const hangmanObj = {
         return console.log('This reveals the blanks of the hidden answers!');
     },
     startGame(x) {
-        this.keyListener();
-        this.documentWriter('usedLetters', this.usedLetterBank);
+        let upperCaseUsed = hangmanObj.usedLetterBank.map( usedLet => usedLet.toUpperCase())
+        this.documentWriter('usedLetters', upperCaseUsed);
         this.documentWriter('gameScore', this.counter);
         this.numberGenerator();
         this.answerSetup(hangmanWord);
+        this.keyListener(this.answerBank);
         this.createUnderscores(hangmanWord);
         this.availableLetterHandler(hangmanObj.letterBank);
     },
-    keyListener() {
+    keyListener(answerBank) {
         document.addEventListener('keypress', (event) => {
-            let keyCode = event.charCode;
-            let character = String.fromCharCode(keyCode);
-            return character; 
-        })
-    },
+         const keyCode = event.charCode;
+         const character = String.fromCharCode(keyCode).toLowerCase();
+         const result = this.answerBank.toString().split("").filter( word => word == character )
+         this.displayAnswerBank.push(result)
+         console.log(result)
+         let replacer = `/${character}/g`
+         console.log(replacer)
+         this.answerBank.toString().replace(replacer, '')
+        return console.log(this.answerBank)
+         console.log(this.displayAnswerBank);
+            })
+        },
     answerSetup(chosenWord){
         let answer = chosenWord; 
+        console.log(answer)
         return this.answerBank.push(answer);
     },
     createUnderscores(chosenWord) {
         let underscores = chosenWord.toString().split("").map(x => '_')
         this.blankBank.push(underscores);
-        document.getElementById("blanksForAnswer").innerText = this.blankBank.map( blanks => blanks.join(' '))
+        this.documentWriter('blanksForAnswer', this.blankBank.map( blanks => blanks.join(' ')));
     },
     numberGenerator() {
        let randomNumber = Math.floor(Math.random() * hangmanObj.wordbank.length);
        return randomNumber;
     },
     availableLetterHandler(letterBank) {
-        document.getElementById("lettersAvailable").innerText = this.letterBank.toLocaleString().toUpperCase();
+        this.documentWriter('lettersAvailable', this.letterBank.toLocaleString().toUpperCase());
+    },
+    answerUpdater(displayAnswerBank, answerBank) {
+
     },
     winChecker() {
         console.log("This Function Checks GuessesLeft");
